86ad6910f1700984e5715c0979b4970722738ec0
AlexisGuadalupe <AlexisOG2009@icloud.com>
2024-09-17 15:46:38 -0600

    Creando cords con un for

--- js/index.js
+++ js/index.js
@@ -1,35 +1,41 @@
-/* <======================= Product 1 ======================> */
+let productContainer = document.getElementById("product-container-title").innerText = ("Card");
+let cardArray = [];
+cardArray.join().replaceAll(",","");
 
-let producNameInsert1 = ("Nike Air force");
-let productPriceInsert1 = ("100$")
-let productDescriptionInsert1 = ("Unos nike air force re facheros que te dal un flow tremendo")
-let imageLink1 = 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/4f37fca8-6bce-43e7-ad07-f57ae3c13142/AIR+FORCE+1+%2707.png'
+/* <=========================== Producto 1 ========================> */
+let title;
+let pricing;
+let description;
 
-let productImage1 = document.getElementById("Image-card-1").src = imageLink1;
-let proctName1 = document.getElementById("Name-card-1").innerText = (producNameInsert1);
-let productPrice1 = document.getElementById("Pricing-card-1").innerText = (productPriceInsert1);
-let productDescription1 = document.getElementById("Description-card-1").innerHTML = (productDescriptionInsert1);
+for (let i = 1; i < 10; i++) {
+    if (i == 1) {
+        title = "BMW M5"
+        pricing = 500
+        description = "xd"
+    } else if (i == 2) {
+        title = "nose"
+        pricing = 4000
+        description = "no se parece un ferrari"
+    } else if (i == 3) {
+        title = "GTA 5"
+        pricing = 50000
+        description = "Pov:"
+    }
+    else{
+        title = "BMW M5"
+        pricing = 500
+        description = "xd"
+    }
 
-/* <======================= Product 2 ======================> */
-
-let producNameInsert2 = ("Lo Kike");
-let productPriceInsert2 = ("3000$")
-let productDescriptionInsert2 = ("Unos nike air force re facheros que te dal un flow tremendo")
-let imageLink2 = 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/4f37fca8-6bce-43e7-ad07-f57ae3c13142/AIR+FORCE+1+%2707.png'
-
-let productImage2 = document.getElementById("Image-card-2").src = imageLink2;
-let proctName2 = document.getElementById("Name-card-2").innerText = (producNameInsert2);
-let productPrice2 = document.getElementById("Pricing-card-2").innerText = (productPriceInsert2);
-let productDescription2 = document.getElementById("Description-card-2").innerHTML = (productDescriptionInsert2);
-
-/* <======================= Product 3 ======================> */
-
-let producNameInsert3 = ("Lo Kike");
-let productPriceInsert3 = ("3000$")
-let productDescriptionInsert3 = ("Unos nike air force re facheros que te dal un flow tremendo")
-let imageLink3 = 'https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/4f37fca8-6bce-43e7-ad07-f57ae3c13142/AIR+FORCE+1+%2707.png'
-
-let productImage3 = document.getElementById("Image-card-3").src = imageLink3;
-let proctName3 = document.getElementById("Name-card-3").innerText = (producNameInsert3);
-let productPrice3 = document.getElementById("Pricing-card-3").innerText = (productPriceInsert3);
-let productDescription3 = document.getElementById("Description-card-3").innerHTML = (productDescriptionInsert3);
+    cardArray.push(`
+        <div class="product-item">
+            <img class="product-img" id="" src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg" alt="">
+            <h3 class="product-title" id="">${title}</h3>
+            <p class="product-pricing" id="">${pricing}</p>
+            <p class="product-description" id="">${description}</p>
+            <div class="produc-button-container">
+                <button class="product-button">Agregar al carrito</button>
+            </div>
+        </div>`);
+    document.getElementById("product-container").innerHTML = cardArray.join().replaceAll(",","");
+}
